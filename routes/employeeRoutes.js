635aefca21b2c75f630a7db0a0a1f5af25a10a55
Yugandhar <boyayugandhar135@gmail.com>
2026-07-13 17:54:45 +0530

    fix issue

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const authenticateToken = require("../middleware/authMiddleware");
+const authenticateToken = require("../middleware/authenticateToken");
 
 const {
   addEmployee,
